16aa30f6a960341650ca32dc3b2a20af9f6222f7
purvimisal <purvivmisal@gmail.com>
2020-10-15 10:21:30 -0700

    newsinlevels scraper

--- newsinlevels-parser.py
+++ newsinlevels-parser.py
@@ -5,32 +5,34 @@ from bs4 import BeautifulSoup
 import re
 import requests
 
-n = 25  #number of pages
 links_dictionary = defaultdict(list)
 
 
 def extract_from_link(link):
-    html_text = requests.get(link).text
-    soup = BeautifulSoup(html_text, 'html.parser')
-    p_tags = soup.find("div", {"id": "nContent"})
-    p_tags = p_tags.find_all('p')
-    text = ''
-    for i in range(1, len(p_tags)-1):
-        if 'Difficult words' not in p_tags[i].renderContents().decode("utf-8"):
-            text += p_tags[i].renderContents().decode("utf-8") +'\n'
-    n = link[-1]
-    link = ''.join(link.split('/')[-1])
-    file_name = '_'.join(link.split('-')[:-2])
-    text = text.replace('<strong>','')
-    text = text.replace('</strong>','')
-    with open(f'level{n}/{file_name}.txt', 'w') as f:
-        f.write(text)
+    try:
+        html_text = requests.get(link).text
+        soup = BeautifulSoup(html_text, 'html.parser')
+        p_tags = soup.find("div", {"id": "nContent"})
+        p_tags = p_tags.find_all('p')
+        text = ''
+        for i in range(1, len(p_tags)-1):
+            if 'Difficult words' not in p_tags[i].renderContents().decode("utf-8"):
+                text += p_tags[i].renderContents().decode("utf-8") +'\n'
+        n = link[-1]
+        link = ''.join(link.split('/')[-1])
+        file_name = '_'.join(link.split('-')[:-2])
+        text = text.replace('<strong>','')
+        text = text.replace('</strong>','')
+        with open(f'level{n}/{file_name}.txt', 'w') as f:
+            f.write(text)
+    except:
+        return 
 
 
 def scrape(i,n):
     for page in range(i,i+n):   # pages to scrape
         vgm_url = f'https://www.newsinlevels.com/page/{page}/'
-        print('Next page')
+        print('Page: ',page)
         html_text = requests.get(vgm_url).text
         soup = BeautifulSoup(html_text, 'html.parser')
         recent_news = soup.find("div", {"class": "recent-news"})
@@ -41,7 +43,13 @@ def scrape(i,n):
                 extract_from_link(link.get('href'))
 
 
+n = 2  #number of pages for each thread
 
-for i in (1,25,50,75):
-    processThread = threading.Thread(target=scrape, args=(i, 25)) # parameters and functions have to be passed separately
+# Using multiple threads
+for i in (55,57,59,61):
+    processThread = threading.Thread(target=scrape, args=(i, n)) # parameters and functions have to be passed separately
     processThread.start()
+
+
+# Not using multiple threads
+# scrape(55,5)
